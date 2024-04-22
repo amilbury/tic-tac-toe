3eb679131dcf95a69c362bdb50ed49d90abd198f
Aaronias4 <111252509+Aaronias4@users.noreply.github.com>
2024-04-21 21:30:09 -0300

    Added a start game button and added its functionality

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ TIC-TAC-TOE GAME
 AUTHOR: AARON MILBURY
 DATE: April 21st, 2024
 */
+//Add event listeners for buttons and cells.
+window.onload = function(){
+    document.querySelector("#startGame").addEventListener("click",startGame);
+}
+
+function startGame(){
+    let content = document.querySelector("#content");
+    content.classList.remove("hidden");
+    let button = document.querySelector("#startGame");
+    button.classList.add("hidden");
+}

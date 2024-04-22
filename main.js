2555696d7e3b816c18abbf73268eee13f4d1a251
Aaronias4 <111252509+Aaronias4@users.noreply.github.com>
2024-04-21 22:58:08 -0300

    Added initial x-o functionality

--- main.js
+++ main.js
@@ -7,10 +7,21 @@ DATE: April 21st, 2024
 window.onload = function(){
     document.querySelector("#startGame").addEventListener("click",startGame);
 }
-
 function startGame(){
     let content = document.querySelector("#content");
     content.classList.remove("hidden");
     let button = document.querySelector("#startGame");
     button.classList.add("hidden");
+    cells = document.querySelectorAll(".cell");
+    for(let i = 0; i < cells.length; i++){
+        cells[i].addEventListener("click",addXO);
+    }
+}
+function addXO(){
+    xo = document.querySelectorAll(".x-o");
+    for(let i = 0; i < xo.length; i++){
+        if(true == true){
+            xo[i].innerHTML = "X"
+        }
+    }
 }
